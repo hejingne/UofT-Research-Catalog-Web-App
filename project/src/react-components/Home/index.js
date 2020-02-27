2497c61e71a0c0b39eb3ca72a4afea7ae79dc612
Yanna Ding <yannading10@gmail.com>
2020-02-27 09:00:07 -0500

    Added simple styles to home page

--- project/src/react-components/Home/index.js
+++ project/src/react-components/Home/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 import Header from "../Header/index"
+import "./styles.css";
 
 // import "./styles.css";
 
@@ -9,7 +12,14 @@ import Header from "../Header/index"
 class Home extends React.Component {
     render() {
         return (
-            <div>
+            <div className="center" id='prompt'>
+                Please select your user type to login: 
+    
+                <ul>
+                    <li><Button className="login__button center">STUDENT</Button></li>
+                    <li><Button className="login__button center">RESEARCHER</Button></li>
+                    <li><Button className="login__button center">ADMINISTRATOR</Button></li>
+                </ul>
             </div>
         );
     }
