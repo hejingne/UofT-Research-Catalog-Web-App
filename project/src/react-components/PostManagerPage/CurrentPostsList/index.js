1f23c990eaa4305d3104e8f85526fd6daca6023c
hejingne <57376488+hejingne@users.noreply.github.com>
2020-03-02 18:16:22 -0500

    Create index.js

---
+++ project/src/react-components/PostManagerPage/CurrentPostsList/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+// import {uid} from "react-uid";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+import Post from "./../Post";
+
+import "./styles.css";
+
+/* Component for the List of CurrentPosts */
+class CurrentPostList extends React.Component {
+  render() {
+    const { posts, component } = this.props;
+
+    /* iterate through the post list and make an <li> for each post*/
+    return (
+      <Table className="post-list">
+        <TableBody>
+          <h2> Current Research Postings: </h2>
+          <TableRow className="post">
+            <TableCell component="th" scope="row">Area Of Study</TableCell>
+            <TableCell component="th" scope="row">Term</TableCell>
+            <TableCell component="th" scope="row">Post ID</TableCell>
+            <TableCell component="th" scope="row">Title</TableCell>
+            <TableCell component="th" scope="row">Deadline</TableCell>
+          </TableRow>
+
+          {posts.map(post => (
+            <Post
+              // key={uid(post)}
+              post={post}
+              component={component}
+            />
+          ))}
+        </TableBody>
+      </Table>
+    );
+  }
+}
+
+export default CurrentPostList;
