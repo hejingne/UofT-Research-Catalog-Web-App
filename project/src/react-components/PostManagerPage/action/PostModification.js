66eb57272e5f25bfe6039ca2cec1389fd486adcb
hejingne <57376488+hejingne@users.noreply.github.com>
2020-03-03 21:49:33 -0500

    Update PostModification.js

--- project/src/react-components/PostManagerPage/action/PostModification.js
+++ project/src/react-components/PostManagerPage/action/PostModification.js
@@ -2,10 +2,16 @@ export const addPost = allPosts => {
 
   const postList = allPosts.state.posts;
 
+  allPosts.setState({
+    id: allPosts.state.id+1
+  });
+
   const post = {
     title: allPosts.state.title,
     areaOfStudy: allPosts.state.areaOfStudy,
-    deadline: allPosts.state.deadline
+    deadline: allPosts.state.deadline,
+    positions: allPosts.state.positions,
+    id: allPosts.state.id
   };
 
   postList.push(post);
