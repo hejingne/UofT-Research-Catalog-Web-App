182f96c22b31e0697bd73067fc38148a65da219a
William Yu <leedsyu@yahoo.com>
2020-03-05 15:16:19 -0500

    db implement

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 
 mongoose
     .connect(
-        "mongodb+srv://server:server@csc309-uoftresearchcatalogue-sql0-gzdxk.mongodb.net/test?retryWrites=true&w=majority",
-        { useNewUrlParser: true, useUnifiedTopology: true }
+        "mongodb+srv://server:server@csc309-uoftresearchcatalogue-sql0-gzdxk.mongodb.net/uoftresearchcatalogue",
+        {useNewUrlParser: true, useUnifiedTopology: true}
     )
     .catch(e => {
         console.error("Connection error", e.message);
     });
-
 const db = mongoose.connection;
 
 module.exports = db;
--- server/src/index.js
+++ server/src/index.js
@@ -3,18 +3,23 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const db = require("./database");
+const profileRouter = require('./routes/profile-router')
+
 
 const app = express();
 const apiPort = 3001;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use(bodyParser.json());
 
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on("error", (error) => console.log("MongoDB connection error: " + error));
+db.once("open", () => console.log("connected to database"));
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+app.use('/api', profileRouter)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
