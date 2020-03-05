9c8d364e7b021f89754aa8e50f275eca80d47823
William Yu <leedsyu@yahoo.com>
2020-03-05 10:53:44 -0500

    bulding backend

---
+++ server/src/database/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://server:server@csc309-uoftresearchcatalogue-sql0-gzdxk.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
+
+const db = mongoose.connection
+
+module.exports = db
